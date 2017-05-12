5ed95063ca7d3d48431ccb00e691d5331e72ae2d
Clint McMahon <clint.mcmahon@gmail.com>
2017-05-11 20:35:04 -0500

    removed unused css files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Matches from './components/Match/Matches';
+import logo from './images/tapin-logo.png';
 class App extends Component {
   render() {
     return (
-      <div className="w-container">
-        <Matches />
+      <div className="container-fluid">
+        <div className="logoname">
+          <img alt="Tapin Guide Logo" src={logo} /> <span className="headcolor">The Vital Matches</span>
+        </div>
+        <div className="w-container">
+          <Matches />
+        </div>
       </div>
     );
   }
