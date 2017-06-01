dc48178c2dbde3b871dcf2cee3170254a44c2765
Clint McMahon <clint.mcmahon@gmail.com>
2017-05-31 21:38:23 -0500

    added color picker

--- src/components/Wallpaper/Wallpaper.js
+++ src/components/Wallpaper/Wallpaper.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import reactCSS from 'reactcss'
+import { SketchPicker } from 'react-color'
+
 import Crest from './Components/Crest';
 
 import './css/wallpaper.css';
@@ -15,11 +18,17 @@ class Backgrounds extends Component {
     super(props);
     
     this.state = {
-      clubs: [],
-      loaded: false,
-      crestUrl: ''
+        clubs: [],
+        loaded: false,
+        crestUrl: '',
+        displayColorPicker: false,
+        color: {
+            r: '56',
+            g: '152',
+            b: '246',
+            a: '100',
+        },
     };
-
   }
 
   setWallpaper(url) {
@@ -53,6 +62,18 @@ class Backgrounds extends Component {
       });
   }
 
+handleClick = () => {
+    this.setState({ displayColorPicker: !this.state.displayColorPicker })
+  };
+
+  handleClose = () => {
+    this.setState({ displayColorPicker: false })
+  };
+
+  handleChange = (color) => {
+    this.setState({ color: color.rgb })
+  };
+
   render() {
     var _this = this;
     var crests = [];
@@ -64,73 +85,112 @@ class Backgrounds extends Component {
       backgroundImage : "url(" + this.state.crestUrl + ")" 
     };
 
+     var backgroundStyle = {
+      backgroundColor : "#" + this.state.color
+    };
+
     var deviceStyle = {
       backgroundImage : "url(" + phone + ")" 
     };
 
+    const styles = reactCSS({
+      'default': {
+        color: {
+          backgroundColor: `rgba(${ this.state.color.r }, ${ this.state.color.g }, ${ this.state.color.b }, ${ this.state.color.a })`,
+        },
+        swatch: {
+          padding: '5px',
+          background: '#fff',
+          borderRadius: '1px',
+          boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+          display: 'inline-block',
+          cursor: 'pointer',
+        },
+        popover: {
+          position: 'absolute',
+          zIndex: '2',
+        },
+        cover: {
+          position: 'fixed',
+          top: '0px',
+          right: '0px',
+          bottom: '0px',
+          left: '0px',
+        },
+      },
+    });
+
     var content = (
         <div className="body">
-              <div className="_2 context">
-              <a className="link-block-2 w-inline-block" href="/about">
-                  <div className="text-block">ABOUT this project</div>
-              </a>
-              <a className="link-block-3 w-inline-block" href="/contact">
-                  <div className="text-block">contact us</div>
-              </a>
+             <div className="_2 context">
+    <a className="link-block-2 w-inline-block" href="/about">
+      <div className="text-block">ABOUT this project</div>
+    </a>
+    <a className="link-block-3 w-inline-block" href="/contact">
+      <div className="text-block">contact us</div>
+    </a>
+  </div>
+  <div className="context">
+    <div className="text-block-2">Pick your team, color and phone and we'll make you a background image.</div>
+  </div>
+  <div className="bodycontainer">
+    <div className="menu" data-ix="menu">
+      <a className="link-block w-inline-block" href="#"><img className="logo" src={logo} />
+      </a>
+    </div>
+    <div className="maincontent">
+      <div className="device" style={deviceStyle}>
+        <div className="div-block-11" style={ styles.color }>
+            <div className="crest selected-crest" style={backgroundCrestStyle}></div>
+          <div className="div-block-8">
+            <div className="div-block-6" style={ styles.color } onClick={ this.handleClick }>
+                
+            </div>
+                { this.state.displayColorPicker ? <div style={ styles.popover }>
+                <div style={ styles.cover } onClick={ this.handleClose }/>
+                <SketchPicker color={ this.state.color } onChange={ this.handleChange } />
+                </div> : null }
+
+            <div className="div-block-7">Background Color</div>
           </div>
-          <div className="context">
-              <div className="text-block-2">Pick your team, color and phone and we'll make you a background image.</div>
-          </div>
-          <div className="bodycontainer">
-              <div className="menu" data-ix="menu">
-                  <a className="link-block w-inline-block" href="/">
-                    <img className="logo" src={logo} alt="Logo" />
-                  </a>
-              </div>
-              <div className="maincontent">
-              <div className="device" style={deviceStyle}>
-                <div className="div-block-8 crest selected-crest" style={backgroundCrestStyle}></div> 
-                <div className="div-block-8">
-                  <div className="div-block-6"></div>
-                  <div className="div-block-7">Background Color</div>
-                </div>
-              </div>
-                  <div className="w-form">
-                      <form data-name="Email Form 2" id="email-form-2" name="email-form-2">
-                          <select className="select-field w-select" id="field" name="field">
-                              <option value="">Select your phone</option>
-                              <option value="First">First Choice</option>
-                              <option value="Second">Second Choice</option>
-                              <option value="Third">Third Choice</option>
-                          </select>
-                          <input className="submit-button-2 w-button" data-wait="Please wait..." type="submit" value="Download"></input>
-                      </form>
-                      <div className="w-form-done">
-                          <div>Thank you! Your submission has been received!</div>
-                      </div>
-                      <div className="w-form-fail">
-                          <div>Oops! Something went wrong while submitting the form</div>
-                      </div>
-                  </div>
-                  <div className="div-block-10"></div>
-              </div>
-              <div className="div-block-5">
-                  <div className="form-wrapper w-form">
-                      <form className="form" data-name="Email Form" id="email-form" name="email-form">
-                          <input className="text-field w-input" data-name="Email" id="email" maxLength="256" name="email" placeholder="Find your team" type="text"></input>
-                      </form>
-                      <div className="w-form-done">
-                          <div>Thank you! Your submission has been received!</div>
-                      </div>
-                      <div className="w-form-fail">
-                          <div>Oops! Something went wrong while submitting the form</div>
-                      </div>
-                  </div>
-              </div>
-              <div className="sidebar">
-                  {crests}
-              </div>
-          </div>
+        </div>
+      </div>
+      <div className="w-form">
+        <form data-name="Email Form 2" id="email-form-2" name="email-form-2">
+          <select className="select-field w-select" id="field" name="field">
+            <option value="">Select your phone</option>
+            <option value="First">First Choice</option>
+            <option value="Second">Second Choice</option>
+            <option value="Third">Third Choice</option>
+          </select>
+          <input className="submit-button-2 w-button" data-wait="Please wait..." type="submit" value="Download"></input>
+        </form>
+        <div className="w-form-done">
+          <div>Thank you! Your submission has been received!</div>
+        </div>
+        <div className="w-form-fail">
+          <div>Oops! Something went wrong while submitting the form</div>
+        </div>
+      </div>
+      <div className="div-block-10"></div>
+    </div>
+    <div className="div-block-5">
+      <div className="form-wrapper w-form">
+        <form className="form" data-name="Email Form" id="email-form" name="email-form">
+          <input className="text-field w-input" data-name="Email" id="email" maxLength="256" name="email" placeholder="Find your team" type="text"></input>
+        </form>
+        <div className="w-form-done">
+          <div>Thank you! Your submission has been received!</div>
+        </div>
+        <div className="w-form-fail">
+          <div>Oops! Something went wrong while submitting the form</div>
+        </div>
+      </div>
+    </div>
+    <div className="sidebar">
+      {crests}
+    </div>
+  </div>
         </div>
     );
     return (
