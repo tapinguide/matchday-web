ea62afe3afceb4fc83c265c681ed39fee00c8a31
Alex Martinez <amart1892@gmail.com>
2018-03-18 21:32:01 -0400

    Added 'Must Wear' and 'Must Listen' blocks

--- src/screens/Matches.js
+++ src/screens/Matches.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Loader from 'react-loader';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Loader from 'react-loader'
 
 // Components
-import Header from '../components/Header';
-import Footer from '../components/Footer/Footer';
-import Match from '../components/Match/Match';
-import MustReadWatch from '../components/MustReadWatch/MustReadWatch';
+import Header from '../components/Header'
+import Footer from '../components/Footer/Footer'
+import Match from '../components/Match/Match'
+import MustReadWatch from '../components/MustReadWatch/MustReadWatch'
 
-const readWatchUrl = "https://api.tapinguide.com/mustreadwatch/?format=json";
+const readWatchUrl = "https://api.tapinguide.com/mustreadwatch/?format=json"
 
 
 export default class Matches extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       matches: [],
       readWatch: [],
@@ -21,22 +21,22 @@ export default class Matches extends Component {
       loaded: false,
       matchDateRange: '',
       parentLoaded: false
-    };
+    }
   }
 
   componentDidMount() {
-    this.setStateFromProps(this.props);
+    this.setStateFromProps(this.props)
 
     this.timerID = setInterval(
       () => this.props.updateMatches(),
       5000
-    );
-    this.props.updateMatches();
+    )
+    this.props.updateMatches()
     this.updateReadWatch()
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setStateFromProps(nextProps);
+    this.setStateFromProps(nextProps)
   }
 
   setStateFromProps(props) {
@@ -45,7 +45,7 @@ export default class Matches extends Component {
       contextBlurb,
       matchDateRange,
       parentLoaded
-    } = props;
+    } = props
 
     this.setState({
       matches,
@@ -56,21 +56,21 @@ export default class Matches extends Component {
   }
 
   componentWillUnmount() {
-    clearInterval(this.timerID);
+    clearInterval(this.timerID)
   }
 
   updateReadWatch() {
-    var _this = this;
+    var _this = this
     axios.all([
       this.getReadWatch()
       ]).then(axios.spread(function (readWatchResult) {
-        var readWatch = readWatchResult.data;
+        var readWatch = readWatchResult.data
 
          _this.setState({
           readWatch: readWatch,
           loaded: true
-        });
-      }));
+        })
+      }))
   }
 
   getReadWatch(){
@@ -79,30 +79,36 @@ export default class Matches extends Component {
 
   render() {
     if(this.state.loaded){
-      var columnLeft = [];
-      var columnRight = [];
+      var columnLeft = []
+      var columnRight = []
       this.state.matches.forEach((match, i) => {
         // Add all matches to left column
-        columnLeft.push(<Match minHeight={this.state.minHeight} setMinHeight={this.setMinHeight} match={match} key={match.id} matchIndex={i} />);
-        columnRight.push(<Match minHeight={this.state.minHeight} setMinHeight={this.setMinHeight} match={match} key={match.id} matchIndex={i} />);
-      });
+        columnLeft.push(<Match minHeight={this.state.minHeight} setMinHeight={this.setMinHeight} match={match} key={match.id} matchIndex={i} />)
+        columnRight.push(<Match minHeight={this.state.minHeight} setMinHeight={this.setMinHeight} match={match} key={match.id} matchIndex={i} />)
+      })
 
-      var readWatch = this.state.readWatch;
+      var readWatch = this.state.readWatch
       if(readWatch.length > 0){
-      var mustReadLeft;
-      var mustReadRight;
+        // Order: Read, Watch, Listen, Wear
+        let mustRead = readWatch[0]
+        let mustWatch = readWatch[1]
+        let mustListen = readWatch[2]
+        let mustWear = readWatch[3]
 
-      mustReadLeft = readWatch[0];
-      mustReadRight = readWatch[1];
+        // Column Left
+        columnLeft.push(<MustReadWatch link={mustRead} key="mustRead" additionalClass="content-highlight" />)
+        columnLeft.push(<MustReadWatch link={mustWatch} key="mustWatch" additionalClass="content-highlight hide-desktop" />)
+        columnLeft.push(<MustReadWatch link={mustListen} key="mustListen" additionalClass="content-highlight" />)
+        columnLeft.push(<MustReadWatch link={mustWear} key="mustWear" additionalClass="content-highlight hide-desktop" />)
 
-      columnLeft.push(<MustReadWatch link={mustReadLeft} key="mustRead" additionalClass="must-read" />);
-      columnLeft.push(<MustReadWatch link={mustReadRight} key="mustWatch" additionalClass="must-watch" />);
-      columnRight.push(<MustReadWatch link={mustReadRight} key="mustWatch" additionalClass="must-watch" />);
+        // Column Right
+        columnRight.push(<MustReadWatch link={mustWatch} key="mustWatch" additionalClass="content-highlight hide-mobile" />)
+        columnRight.push(<MustReadWatch link={mustWear} key="mustWear" additionalClass="content-highlight hide-mobile" />)
       }
     }
-    var bigText = "Essential Matches";
+    var bigText = "Essential Matches"
 
-    const { matchDateRange, contextBlurb, loaded, parentLoaded } = this.state;
+    const { matchDateRange, contextBlurb, loaded, parentLoaded } = this.state
 
     return (
       <div className="wrapper wrapper-matches">
@@ -123,7 +129,7 @@ export default class Matches extends Component {
           </div>
         </Loader>
       </div>
-    );
+    )
   }
 }
 
